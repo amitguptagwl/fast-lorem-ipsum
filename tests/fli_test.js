8c44cf740c50cc7968d43ad6dd0e6d34f2d87755
Amit Gupta <amitgupta.gwl@gmail.com>
2018-02-23 09:23:01 +0530

    Add tests and browser bundle file

---
+++ tests/fli_test.js
@@ -0,0 +1,22 @@
+var fli = require("../index").fastLoremIpsum;
+
+describe("Lorem Ipsum Generator", function () {
+
+    it("should generate lorem ipsum N chars long string ", function () {
+        var str = fli("10c");
+        var expected = "lorem ipsu";
+        expect(str).toEqual(expected);
+
+        var str = fli(10,"c");
+        expect(str).toEqual(expected);
+    });
+
+    it("should generate lorem ipsum N words long string ", function () {
+        var str = fli("10w");
+        var expected = "lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam";
+        expect(str).toEqual(expected);
+
+        var str = fli(10,"w");
+        expect(str).toEqual(expected);
+    });
+});
